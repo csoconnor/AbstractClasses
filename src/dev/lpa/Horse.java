c83637b9710d4d586952143a2d418a94d5069295
Colton O'Connor <109178036+csoconnor@users.noreply.github.com>
2023-07-21 14:13:09 -0600

    Push to github

---
+++ src/dev/lpa/Horse.java
@@ -0,0 +1,23 @@
+package dev.lpa;
+
+public class Horse extends Mammal{
+
+    public Horse(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void shedHair() {
+        System.out.println(getExplicitType() + " sheds in the spring");
+    }
+}
---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,42 @@
+package dev.lpa;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+//        Animal animal = new Animal("animal", "big", 100);
+
+        Dog dog = new Dog("Wolf", "big", 100);
+        dog.makeNoise();
+
+        doAnimalStuff(dog);
+        System.out.println();
+
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(new Dog("German Shepard", "big", 150));
+        animals.add(new Fish("Goldfish", "small", 1));
+        animals.add(new Fish("Barracuda", "big", 75));
+        animals.add(new Dog("Pug", "small", 20));
+
+        animals.add(new Horse("Clydesdale", "large", 1000));
+
+        for(Animal animal: animals){
+            doAnimalStuff(animal);
+            if(animal instanceof Mammal currentMammal){
+                currentMammal.shedHair();
+            }
+        }
+    }
+
+    private static void doAnimalStuff(Animal animal) {
+        animal.makeNoise();
+        animal.move("slow");
+    }
+
+
+
+
+}
